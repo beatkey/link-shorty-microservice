0dfc209803c1d42915698d095c90040818b56eda
Emre Cal <emrecaal@gmail.com>
2022-10-15 02:07:21 +0300

    tinyurl provider done

--- app/Http/Controllers/Api/ShortLinksController.php
+++ app/Http/Controllers/Api/ShortLinksController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\ShortLinksRequest;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class ShortLinksController extends Controller
 {
-    public function index(Request $request){
-        dd($request->all());
+    public function index(ShortLinksRequest $request){
+        $client = new Client();
+
+        $res = $client->post("https://api.tinyurl.com/create", [
+            "query" => [
+                "api_token" => env("TINYURL_API")
+            ],
+            "json" => [
+                "url" => $request->url
+            ]
+        ]);
+
+        if ($res->getStatusCode() !== 200) {
+            return response()->json([
+                "error" => "Remote server error: " . $res->getReasonPhrase(),
+            ], 400);
+        }
+
+        $res = json_decode($res->getBody())->data;
+
+        return response()->json([
+            "url" => $request->url,
+            "link" => $res->tiny_url
+        ]);
     }
 }
---
+++ app/Http/Requests/ShortLinksRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+    namespace App\Http\Requests;
+
+    use Illuminate\Contracts\Validation\Validator;
+    use Illuminate\Foundation\Http\FormRequest;
+    use Illuminate\Http\Exceptions\HttpResponseException;
+    use Illuminate\Validation\Rule;
+
+    class ShortLinksRequest extends FormRequest
+    {
+        /**
+         * Determine if the user is authorized to make this request.
+         *
+         * @return bool
+         */
+        public function authorize(){
+            return true;
+        }
+
+        /**
+         * Get the validation rules that apply to the request.
+         *
+         * @return array<string, mixed>
+         */
+        public function rules(){
+            return [
+                "url" => [
+                    "required",
+                    "url",
+                ],
+                "provider" => [
+                    Rule::in(["bit.ly", "tinyurl"])
+                ]
+            ];
+        }
+
+        protected function failedValidation(Validator $validator){
+            throw new HttpResponseException(response()->json([
+                "errors" => $validator->errors(),
+            ], 422));
+        }
+    }
