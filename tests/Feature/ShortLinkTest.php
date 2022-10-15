a4c9c7f13cc4c7aaa4905095d343aa52b8556daf
Emre Cal <emrecaal@gmail.com>
2022-10-15 03:00:22 +0300

    providers test created

---
+++ tests/Feature/ShortLinkTest.php
@@ -0,0 +1,34 @@
+<?php
+
+    namespace Tests\Feature;
+
+    use Illuminate\Foundation\Testing\RefreshDatabase;
+    use Illuminate\Foundation\Testing\WithFaker;
+    use Illuminate\Testing\Fluent\AssertableJson;
+    use Tests\TestCase;
+
+    class ShortLinkTest extends TestCase
+    {
+        /**
+         * A basic feature test example.
+         *
+         * @return void
+         */
+        public function test_create_short_link_tinyurl(){
+            $response = $this->post('/api/v1/shortlinks', [
+                "url" => "https://example.com",
+                "provider" => "tinyurl"
+            ]);
+
+            $response->assertStatus(200);
+        }
+
+        public function test_create_short_link_bitly(){
+            $response = $this->post('/api/v1/shortlinks', [
+                "url" => "https://example.com",
+                "provider" => "bit.ly"
+            ]);
+
+            $response->assertStatus(200);
+        }
+    }
